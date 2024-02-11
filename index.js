7c53afe00c14cb43c7b6a9b6dfd4a46083e9a604
Paul Donovan <paul.m.donovan@outlook.com>
2024-02-11 13:37:52 +1100

    addsMathLab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = (num1 * num2);
+console.log(multiply)
+
+let random =
+    Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+const num3 = 20;
+const num4 = 16;
+let mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max(1, 2, 3, 4, 20);
+console.log(max)
